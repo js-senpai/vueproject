847b58c6129efa5a9b70ca6f059cc25316bd6a76
Andrew <andrewkarapenkov@gmail.com>
2019-08-30 20:26:53 +0300

    Create banner in header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
   <div id="app">
-
+     <header-template></header-template>
   </div>
 </template>
-
 <style lang="sass">
 @import './sass/main.sass'
 </style>
+<script>
+import Header from './components/Header'
+export default{
+  name: 'app',
+  data(){
+    return {
+
+    }
+  },
+  components:{
+    'header-template':Header
+  }
+}
+</script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,62 @@
 <template>
-
+ <nav class="nav-menu">
+     <div class="container flex-container">
+         <div class="logo">
+             <a href="#"><img src="../assets/logo/logo.svg" alt="logo"></a>
+         </div>
+         <ul class="menu flex-container">
+             <li class="menu-item" v-for="(menuItem,index) in menu" :key="index"><a :href="menuItem.url">{{menuItem.pageName}}</a></li>
+         </ul>
+         <div class="login-container flex-container">
+             <div class="login-contact">
+                 <span class="login-name">
+                   {{currentUser.name}}
+                 </span>
+                 <span class="login-email">
+                   {{currentUser.email}}
+                 </span>
+             </div>
+             <div class="login-avatar">
+                 <a href="#"><img :src="currentUser.photo" alt="login"></a>
+             </div>
+             <a href="#" class="logout">
+             </a>
+         </div>
+     </div>
+ </nav>
 </template>
+<script>
+    import axios from 'axios'
+    export default {
+        data(){
+            return {
+                menu: [
+                    {url: '#',pageName: 'About me'},
+                    {url: '#',pageName: 'Relationships'},
+                    {url: '#',pageName: 'Requirements'},
+                    {url: '#',pageName: 'Users'},
+                    {url: '#',pageName: 'Sign Up'}
+                ],
+                currentUser: {
+                    name: '',
+                    photo: '',
+                    email: ''
+                }
+            }
+        },
+        created() {
+           axios({
+               methods: 'get',
+               url: 'https://frontend-test-assignment-api.abz.agency/api/v1/users/1'
+           })
+               .then(response =>{
+                   this.currentUser.name = response.data.user.name;
+                   this.currentUser.email = response.data.user.email;
+                   this.currentUser.photo = response.data.user.photo;
+               })
+               .catch(e => {
+                   console.log(e)
+               })
+        }
+    }
+</script>
